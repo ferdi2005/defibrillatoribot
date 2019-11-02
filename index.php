ed6e299dd96618865c03e426496c8be0f771a23a
Ferdinando Traversa <ferdi.traversa@gmail.com>
2019-11-03 00:45:30 +0100

    fix stilistici, rimuovo file inutile

--- index.php
+++ index.php
@@ -12,31 +12,32 @@
 
 <body>
 
-<div style="text-align: center;">
+<div class="section" style="text-align: center;">
   <h1 class="title is-1">
     Trova Defibrillatori
   </h1>
   <h2 class="subtitle">Mappa globale e costruita dagli utenti dei defibrillatori</h2>
-</div>
 
 <div class="buttons is-centered">
   <a class="button is-large is-primary" href="https://trovadefibrillatori.it/locate.php">Localizza ora i defibrillatori vicino a te</a>
 </div>
-<h1 class="subtitle is-danger">
+<h1 class="title is-3 is-danger">
 Perché TrovaDefibrillatori  
 </h1>
+<h2 class="title is-4">Italiano</h2>
 In caso di arresto cardiaco la prima cosa da fare è chiamare il 112 (in Europa) e per ora il 118 in Italia. ogni minuto di mancata assistenza, fa perdere il 10% di possibilità di sopravvivenza. Ecco perchè dopo aver chiamato i soccorsi, è opportuno avere un Defibrillatore e un volontario formato nei paraggi. Ma dove trovare un DAE?
 Questo sito web ti indica i Defibrillatori presenti su <a href="https://openstreetmap.org">OpenStreetMap</a> attorno alla tua posizione.
 
 <br />
 Video sulla procedura BLSD: <a href="https://www.youtube.com/watch?v=vnS_PNh0oDk">https://www.youtube.com/watch?v=vnS_PNh0oDk</a>
 <hr/>
+<h2 class="title is-4">English</h2>
 In case of cardiac arrest, the first thing to do is call 112 (in Europe) and now the 118 in Italy. every minute of no assistance, lose 10% chance of survival. That is why after having called for help, you should have a defibrillator and a volunteer form around. But where to find an AED?
 This website tells you the defibrillators on <a href="https://openstreetmap.org">OpenStreetMap</a> around your location. Just click "Localizza ora i defibrillatori vicino a te".
 <br />
 Video BLSD procedure: <a href="https://www.youtube.com/watch?v=vnS_PNh0oDk">https://www.youtube.com/watch?v=vnS_PNh0oDk</a>
 
-<h1 class="subtitle is-danger">
+<h1 class="title is-3">
 Come inserisco i defibrillatori su questo sito?  
 </h1>
 <a href="https://openstreetmap.org">OpenStreetMap</a> è un database globale di informazioni geografiche realizzato dagli utenti. I DAE sono codificati con il tag <b>emergency=defibrillator</b> e sempre consigliabile <b>name=nome del luogo</b>. Questo sito attinge proprio da OpenStreetMap.
@@ -55,8 +56,8 @@ Una volta scaricata Go Map!, segui questi passaggi:
 <li>Inviare tramite il pulsante “nuvola” la modifica effettuata inserendo una nota per propria comodità</li>
 <li>opzionalmente si possono aggiungere anche altri tags come address, phone, website ect. Infatti tale procedura vale per qualsiasi oggetto non solo per i DAE.</li>
 </ol>
-<img src="/gomapguide.png" width="200px" />
-<h2 class="subtitle is-4">Android</h2>
+<img src="/gomapguide.png" width="500px" />
+<h2 class="title is-5">Android</h2>
 Usa l'applicazione <b>Vespucci</b>, che puoi scaricare usando <a href="https://play.google.com/store/apps/details?id=de.blau.android&hl=it
 ">questo link</a>. Segui quindi i seguenti passaggi:
 <ol>
@@ -67,8 +68,10 @@ Usa l'applicazione <b>Vespucci</b>, che puoi scaricare usando <a href="https://p
 <li>Inserire i tags <b>emergency=defibrillator</b> e <b>name=nome luogo</b>.</li>
 <li>Inviare tramite il pulsante di sincronizzazione -> carica dati nel server osm</Li>
 <li>opzionalmente si possono aggiungere anche altri tags come address, phone, website ect. Infatti tale procedura vale per qualsiasi oggetto non solo per i DAE.</li>
-<img src="/vespucciguide.png" width="200px" />
+</ol>
+<img src="/vespucciguide.png" width="500px" />
 
+</div>
 
 <footer class="footer">
   <div class="content has-text-centered">
